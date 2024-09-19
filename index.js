82d63d2841b50138c2f04ab06c407d0333154315
williamscodigo <willliamscodigo@gmail.com>
2024-09-19 14:23:23 -0400

    completed all requirements for challange

--- index.js
+++ index.js
@@ -1,13 +1,132 @@
 // TODO: Include packages needed for this application
+import inquirer from 'inquirer';
+import fs from 'node:fs';
+import { renderLicenseBadge, renderLicenseSection } from './utils/generateMarkdown.js';
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [ 
+    {
+    type: 'input',
+    name: 'title',
+    message: 'What is your app/project title?'
+   },
+   {
+    type: 'input',
+    name: 'description',
+    message: 'What is your app/project description?',
+   },
+   {
+    type: 'input',
+    name: 'installation',
+    message: 'Write the installation process for your application!?',
+   },
+   {
+    type: 'input',
+    name: 'usage',
+    message: 'How to use your application!?',
+   },
+   {
+    type: 'input',
+    name: 'contribute',
+    message: 'How to contribute?',
+   },
+   {
+    type: 'input',
+    name: 'test',
+    message: 'How to test your application?',
+   },
+   {
+    type: 'list',
+    name: 'license',
+    message: 'What license would you choose?',
+    choices: ['MIT', 'ISC', 'APACHEv2', 'OTHER']
+   },
+   {
+    type: 'input',
+    name: 'username',
+    message: 'What is your github profile link?',
+   },
+   {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email?',
+   },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, answers) {
+    let readmeContent = 
+    //TODO: make badge clickable - were to go license section in readme or actual license website
+`# ${answers.title} ${renderLicenseBadge(answers.license)}
+
+## Description
+
+${answers.description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [How to Contribute](#how-to-contribute)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+${answers.installation}
+
+## Usage
+
+${answers.usage}
+
+## How to Contribute
+
+${answers.contribute}
+
+## Tests
+
+${answers.test}
+
+${renderLicenseSection(answers.license)}
+
+## Questions
+
+GitHub Link: [${answers.username}](${answers.username})
+
+
+Reach me with additional questions at: ${answers.email}
+`;
+
+    fs.writeFile(fileName, readmeContent, (err) => {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log('file written successfully ✅');
+        }
+      })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then(answers => {
+        //console.log(answers);
+        writeToFile('README.md', answers);
+    })
+}
 
 // Function call to initialize app
 init();
+
+
+//////////////////////////////////////////////////////////////////////////////
+
+/*
+
+## TODO:
+### NOTE: WE SHOULD HAVE A WAY TO MAKE SOME OF THIS SECTIONS OPTIONAL
+### IN THIS STRING Literal WE WILL AN if (provided) conditional
+### on prompt side, we will have a quenstion  like/example 'WOULD YOU LIKE TO PROVIDE A how-to-contribute SECTION TO YOUR README FILE? - continue prompt base on answer, or quit and generate README with given information
+### BETTER IDEA - PROMT USER TO CHECK BOXES WITH ALL SECTIONS THEY WANT THEIR README TO HAVE AND THEM PORMPT USER only FOR THAT INFORMATION - NOT SURE IF SETUP WILL WORK FINE WITH THIS SOLUTION APPROACH
+
+*/
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,49 @@
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let licenseBadge = '';
+  switch(license){
+    case 'MIT':
+      licenseBadge = '![Static Badge](https://img.shields.io/badge/license-MIT-blue)';
+      break;
+    case 'ISC':
+      licenseBadge = '![Static Badge](https://img.shields.io/badge/license-ISC-blue)';
+      break;
+    case 'APACHEv2':
+      licenseBadge = '![Static Badge](https://img.shields.io/badge/license-APACHEv2-blue)';
+      break;
+    default:
+      licenseBadge = '';
+  }
+
+  return licenseBadge;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(license === 'MIT') return 'https://opensource.org/license/mit';
+  if(license === 'ISC') return 'https://opensource.org/license/isc-license-txt';
+  if(license === 'APACHEv2') return 'https://www.apache.org/licenses/LICENSE-2.0';
+  return '';
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+ const licenseSection = `## License
+[${renderLicenseLink(license)}](${renderLicenseLink(license)})
+`
+
+return licenseSection;
+}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
+function generateMarkdown(answers) {
+  return `# ${answers.title}
 `;
 }
 
 export default generateMarkdown;
+export { renderLicenseBadge, renderLicenseLink, renderLicenseSection }; 
